e14191004faff4af38c6530a89e0a077428b9ce5
Alberto Montes <alm59321@gmail.com>
2018-04-10 11:27:50 +0200

    :green_heart: Fix

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -4,7 +4,6 @@ python setup.py clean --all
 python setup.py sdist bdist_wheel
 
 # Upload to PyPi test
-twine upload --repository-url "https://test.pypi.org/legacy/" \
-	-u "$PYPI_USER" -p "$PYPI_PASSWORD" dist/*
+twine upload --repository-url "https://test.pypi.org/legacy/" dist/*
 # Upload to PyPi
-twine upload -r pypi -u "$PYPI_USER" -p "$PYPI_PASSWORD" dist/*
+twine upload dist/*
