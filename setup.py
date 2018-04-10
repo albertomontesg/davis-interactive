35e51cbc5663cd7b1d91fcc8bdc9f66e3cbf2681
Alberto Montes <alm59321@gmail.com>
2018-04-10 10:52:14 +0200

    :construction_worker: Add PyPi to CI

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 from setuptools import setup
 
 setup(
-    name='davisinteractive',
-    version='0.0.1',
-    description=
-    'Evaluation tools for DAVIS interactive segmentation challenge.',
-    url='http://github.com/albertomontesg/davis-interactive',
-    author='Alberto Montes',
-    author_email='al.montes.gomez@gmail.com',
-    license='MIT',
-    packages=['davisinteractive'],
     install_requires=[
         'numpy>=1.12.1', 'scikit-learn>=0.18', 'scikit-image>=0.13.1',
         'networkx>=2.0', 'scipy>=1.0.0', 'pandas>=0.21.1', 'absl-py>=0.1.13',
@@ -17,5 +8,4 @@ setup(
     ],
     setup_requires=['pytest-runner'],
     test_require=['pytest', 'pytest-cov'],
-    include_package_data=True,
-    zip_safe=False)
+)
