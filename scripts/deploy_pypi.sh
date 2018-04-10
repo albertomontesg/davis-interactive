f91adee32e79c38c884d0215e3c1669dc159e7b6
Alberto Montes <alm59321@gmail.com>
2018-04-10 12:47:00 +0200

    :green_heart: test

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -3,6 +3,7 @@
 python setup.py clean --all
 python setup.py sdist bdist_wheel
 
+echo "$TWINE_USERNAME"
 # Upload to PyPi test
 twine upload --repository-url "https://test.pypi.org/legacy/" dist/*
 # Upload to PyPi
