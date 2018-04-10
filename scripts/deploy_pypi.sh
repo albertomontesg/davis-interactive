9eb2085652b492c72338f1870ad117830c0da3f9
Alberto Montes <alm59321@gmail.com>
2018-04-10 11:23:09 +0200

    :green_heart: Fix

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 setup.py clean --all
-python3 setup.py sdist bdist_wheel
+python setup.py clean --all
+python setup.py sdist bdist_wheel
 
 # Upload to PyPi test
 twine upload --repository-url "https://test.pypi.org/legacy/" \
-	-u $PYPI_USER -p $PYPI_PASSWORD dist/*
+	-u "$PYPI_USER" -p "$PYPI_PASSWORD" dist/*
 # Upload to PyPi
-twine upload -r pypi -u $PYPI_USER -p $PYPI_PASSWORD dist/*
+twine upload -r pypi -u "$PYPI_USER" -p "$PYPI_PASSWORD" dist/*
