a00eb42f2c4fd291335df661babfe2e8eaec04ed
Alberto Montes <alm59321@gmail.com>
2018-04-11 11:19:59 +0200

    📝 Add more information fot PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,4 @@ setup(
         'numpy>=1.12.1', 'scikit-learn>=0.18', 'scikit-image>=0.13.1',
         'networkx>=2.0', 'scipy>=1.0.0', 'pandas>=0.21.1', 'absl-py>=0.1.13',
         'Pillow>=4.1.1', 'mock;python_version<"3.0"'
-    ],
-    setup_requires=['pytest-runner'],
-    test_require=['pytest', 'pytest-cov', 'mock'],
-)
+    ])
