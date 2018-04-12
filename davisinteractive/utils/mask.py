61584be5e4deff29e31e8bda08d3854bd7a3f1b7
Alberto Montes <alm59321@gmail.com>
2018-04-12 09:09:22 +0200

    📝 Add new functions to documentation

--- davisinteractive/utils/mask.py
+++ davisinteractive/utils/mask.py
@@ -4,12 +4,16 @@ import numpy as np
 def combine_masks(masks, th=0.5, method='max_per_pixel'):
     """ Combine mask for different objects
 
+    Different methods are the following:
+
+    * `max_per_pixel`: Computes
+
     # Arguments
-        masks: List containing a list of masks for every object. Therefore, len(masks) == number_objects
-               and len(masks[0]) == number_frames. The masks should be numpy array.
-        th: Threshold to binarize the masks. Default: 0.5
-        method: Method that specifies how the masks are fuse, the following are available:
-                - max_per_pixel(default): Computes
+        masks: List. Containing a list of masks for every object.
+            Therefore, `len(masks) == number_objects` and
+            `len(masks[0]) == number_frames`. The masks should be Numpy Array.
+        th: Float. Threshold to binarize the masks.
+        method: String. Method that specifies how the masks are fuse.
 
     # Returns
         list: Returns a list with all the results of the masks fused
@@ -18,13 +22,16 @@ def combine_masks(masks, th=0.5, method='max_per_pixel'):
     n_objects = len(masks)
     output_masks = []
     for fr_id in range(n_frames):
-        res_mat = np.zeros((masks[0][fr_id].shape[0], masks[0][fr_id].shape[1], n_objects))
+        res_mat = np.zeros((masks[0][fr_id].shape[0], masks[0][fr_id].shape[1],
+                            n_objects))
         for obj_id in range(n_objects):
             res_mat[:, :, obj_id] = masks[obj_id][fr_id]
         marker = np.argmax(res_mat, axis=2)
-        out_mask = np.zeros((masks[0][fr_id].shape[0], masks[0][fr_id].shape[1]))
+        out_mask = np.zeros((masks[0][fr_id].shape[0],
+                             masks[0][fr_id].shape[1]))
         for obj_id in range(n_objects):
-            tmp_mask = np.logical_and(marker == obj_id, masks[obj_id][fr_id] > th)
-            out_mask[tmp_mask] = obj_id+1
+            tmp_mask = np.logical_and(marker == obj_id,
+                                      masks[obj_id][fr_id] > th)
+            out_mask[tmp_mask] = obj_id + 1
         output_masks.append(out_mask)
     return output_masks
