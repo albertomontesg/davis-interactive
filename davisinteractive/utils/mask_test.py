40ef98f7a0bf04bca90465ea3fcbb0fe59626235
scaelles <scaelles@gmail.com>
2018-04-12 09:56:38 +0200

    Added TEST_DIR path in mask_test

--- davisinteractive/utils/mask_test.py
+++ davisinteractive/utils/mask_test.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from .mask import combine_masks
 
+TEST_DIR = os.path.join(os.path.dirname(__file__), 'masks_test')
+
 
 class TestCombineMasks:
 
@@ -15,13 +17,11 @@ class TestCombineMasks:
         for obj_id in range(n_objects):
             obj_masks = []
             for fr_id in range(n_frames):
-                obj_masks.append(np.array(Image.open(os.path.join(os.path.dirname(__file__),
-                                                                  'masks_test', 'input_masks', str(obj_id+1),
+                obj_masks.append(np.array(Image.open(os.path.join(TEST_DIR, 'input_masks', str(obj_id+1),
                                                                   '{:05d}.png'.format(fr_id)))))
             all_masks.append(obj_masks)
         final_mask = combine_masks(all_masks)
         for fr_id in range(n_frames):
-            gt_mask = np.array(Image.open(os.path.join(os.path.dirname(__file__),
-                                                       'masks_test', 'output_masks',
+            gt_mask = np.array(Image.open(os.path.join(TEST_DIR, 'output_masks',
                                                        '{:05d}.png'.format(fr_id))))
             assert (final_mask[fr_id] == gt_mask).all()
