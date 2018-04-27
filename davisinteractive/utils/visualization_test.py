cfdd4fa9bc6afe11df3a43efabc93903ef1c8466
Alberto Montes <alm59321@gmail.com>
2018-04-27 15:29:42 +0200

    ✅ Add test at visualization module

---
+++ davisinteractive/utils/visualization_test.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, division
+
+import unittest
+
+import numpy as np
+import pytest
+
+from davisinteractive.utils.visualization import (_pascal_color_map,
+                                                  plot_scribble)
+
+import matplotlib  # isort:skip
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt  # isort:skip
+
+
+class TestPascalColormap(unittest.TestCase):
+
+    def test_default(self):
+        colormap = _pascal_color_map()
+        assert colormap.shape == (256, 3)
+        assert colormap.dtype == np.uint8
+
+        assert np.all(colormap[0] == [0, 0, 0])
+        assert np.all(colormap[1] == [128, 0, 0])
+        assert np.all(colormap[2] == [0, 128, 0])
+        assert np.all(colormap[3] == [128, 128, 0])
+        assert np.all(colormap[255] == [224, 224, 192])
+
+    def test_normalized(self):
+        colormap = _pascal_color_map(normalized=True)
+        assert colormap.shape == (256, 3)
+        assert colormap.dtype == np.float32
+
+        assert np.all(np.isclose(colormap[0], [0, 0, 0]))
+        assert np.all(np.isclose(colormap[1], [128 / 255, 0, 0]))
+        assert np.all(np.isclose(colormap[2], [0, 128 / 255, 0]))
+        assert np.all(np.isclose(colormap[3], [128 / 255, 128 / 255, 0]))
+        assert np.all(
+            np.isclose(colormap[255], [224 / 255, 224 / 255, 192 / 255]))
+
+
+class TestPlotScribble(unittest.TestCase):
+
+    def test_plot(self):
+        scribble = {
+            'scribbles': [[], [{
+                'path': [[0, 0], [0, 0.1]],
+                'object_id': 1,
+                'start_time': 0,
+                'end_time': 1000
+            }, {
+                'path': [[0, 0], [0, 0.1]],
+                'object_id': 1,
+                'start_time': 0,
+                'end_time': 1000
+            }], [], [], [{
+                'path': [[0, 0], [0, 0.1]],
+                'object_id': 2,
+                'start_time': 0,
+                'end_time': 1000
+            }, {
+                'path': [[0, 0], [0, 0.1]],
+                'object_id': 3,
+                'start_time': 0,
+                'end_time': 1000
+            }], [{
+                'path': [[0, 0], [0, 0.1]],
+                'object_id': 2,
+                'start_time': 0,
+                'end_time': 1000
+            }], []],
+            'sequence':
+            'test',
+        }
+        _, ax = plt.subplots(1)
+        plot_scribble(ax, scribble, 1, output_size=(100, 200))
+
+        with pytest.raises(ValueError):
+            plot_scribble(ax, scribble, 10)
