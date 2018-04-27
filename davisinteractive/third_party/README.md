c503e7ca654f78a037897c24bcbdef4d7417ff3a
Alberto Montes <alm59321@gmail.com>
2018-04-27 17:02:53 +0200

    ✨ Add cocoapi to third party

---
+++ davisinteractive/third_party/README.md
@@ -0,0 +1,8 @@
+# Third Party
+
+Here is placed some code from [`cocoapi`](https://github.com/cocodataset/cocoapi).
+
+
+
+Copyright (c) 2014, Piotr Dollar and Tsung-Yi Lin
+All rights reserved.
--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from setuptools import setup
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+ext_modules = [
+    Extension(
+        'davisinteractive.third_party._mask',
+        sources=[
+            'davisinteractive/third_party/maskApi.c',
+            'davisinteractive/third_party/_mask.pyx'
+        ],
+        include_dirs=[np.get_include(), 'davisinteractive/third_party'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
 
 setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
@@ -7,4 +21,5 @@ setup(
         'networkx>=2.0', 'scipy>=1.0.0', 'pandas>=0.21.1', 'absl-py>=0.1.13',
         'Pillow>=4.1.1', 'mock;python_version<"3.0"',
         'pathlib2;python_version<"3.5"', 'six>=1.10.0'
-    ])
+    ],
+    ext_modules=cythonize(ext_modules))
